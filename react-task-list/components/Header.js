6d2c54213a41d2ca09db027679a1037f025ae57f
alexandru233847 <alexutualexino33@gmail.com>
2024-11-11 18:56:41 +0100

    Exports

---
+++ react-task-list/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header = () => {
+    const appName = "Task Manager";
+    return (
+        <header>
+            <h1>{appName}</h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ react-task-list/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Main = () => {
+    const welcomeMessage = "Welcome to my React App";
+    return (
+        <main>
+            <h2>{welcomeMessage}</h2>
+        </main>
+    );
+};
+
+export default Main;
+
